deaef9e79f2abbbab9508b1340f9bd2c93a889ca
Eric Tseng <erictseng89@gmail.com>
2021-06-08 02:51:14 +0800

    ep5 - basic web.php and /resources/view directory

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+	// This is actually:
+	// return view('welcome.blade.php');
+	// but the .blade.php is not required for this function.
+});
+
+/*
+Can also return json objects
+
+Route::get('/', function() {
+	$json = ['foo' => 'bar'];
+	return $json;
 });
