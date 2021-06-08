94ab9b8294b1932693a29b27a5c6658246f8cd79
Eric Tseng <erictseng89@gmail.com>
2021-06-08 23:03:24 +0800

    ep23 - changed URI to slug instead of ID using new wildcard post:slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
   @foreach ($posts as $post)
     <article>
       <h1>
-        <a href="/post/{{ $post->id }}">{{ $post->title }}</a>
+        <a href="/post/{{ $post->slug }}">{{ $post->title }}</a>
       </h1>
       {{ $post->excerpt }}
     </article>
--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,14 @@ use function PHPUnit\Framework\fileExists;
 */
 
 Route::get('/', function () {
-	// return view('posts', [
-	// 	'posts' => Post::all()
-	// ]);
-	/* $document = YamlFrontMatter::parseFile(
-		resource_path('posts/my-first-post.html')
-	);
-
-	ddd($document); */
-
 	return view('posts', [
 		'posts' => Post::all()
 	]);
 });
 
-Route::get('/post/{post}', function ($id) {
+Route::get('/post/{post:slug}', function (Post $post) {
 	return view('post', [
-		'post' => Post::findOrFail($id)
+		'post' => $post
 	]);
 });
 
