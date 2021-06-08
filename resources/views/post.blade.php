378bbeb01c2abb46f42b61faf8afe2a89ab4f974
Eric Tseng <erictseng89@gmail.com>
2021-06-08 19:56:48 +0800

    ep15 - simple blade template, blade components using $slot

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-	{{-- This link will refer to the file in the /public folder. --}}
-	<link rel="stylesheet" href="/app.css">
-
-	<title>{{ $post->title }}</title>
-</head>
-<body>
-	<h1>{{ $post->title }}</h1>
-	{!! $post->body !!}	
+<x-layout>
+	<article>
+		<h1>{{ $post->title }}</h1>
+		{!! $post->body !!}
+	</article>
 	<a href="/">Go back</a>
-</body>
-</html>
+</x-layout>
