2a3d168fa51d42a795ca3c460cc7cc2b202e90f1
Eric Tseng <erictseng89@gmail.com>
2021-06-09 20:38:22 +0800

    ep31 - added HTML & CSS, used blade components and partials

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
-  @foreach ($posts as $post)
-    <article>
-      <h1>
-        <a href="/post/{{ $post->slug }}">{{ $post->title }}</a>
-      </h1>
-      <h3>
-        In
-        <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>,
-        By
-        <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
-      </h3>
-      {{ $post->excerpt }}
-    </article>
-  @endforeach
+  @include("_posts-header")
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card />
+    <div class="lg:grid lg:grid-cols-2">
+      <x-post-card />
+      <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+      <x-post-card />
+      <x-post-card />
+      <x-post-card />
+    </div>
+  </main>
 </x-layout>
