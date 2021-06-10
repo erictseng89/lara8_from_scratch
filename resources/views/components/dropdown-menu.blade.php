4f3b8d645d5dec9634687a29814bf8b7f1191075
Eric Tseng <erictseng89@gmail.com>
2021-06-10 20:12:34 +0800

    ep36 - implode arrays for seeding, escape variables for html tags, tailwind spacing space-x-4

--- resources/views/components/dropdown-menu.blade.php
+++ resources/views/components/dropdown-menu.blade.php
@@ -6,7 +6,8 @@
     {{ $trigger }}
   </div>
   {{-- Links --}}
-  <div x-show="show" class="py-2 absolute bg-gray-100 rounded-xl w-full mt-2 z-30" style="display:none">
+  <div x-show="show" class="py-2 absolute bg-gray-100 rounded-xl w-full mt-2 z-30 overflow-auto max-h-52"
+    style="display:none">
     {{ $slot }}
   </div>
 </div>
